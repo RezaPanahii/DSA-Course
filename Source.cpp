1c2ea0b9d34f7884baa6506a6403452c33053a55
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-17 16:26:22 +0430

    initialize, input & output arrays

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,25 @@
 
 int main()
 {
-	std::cout << "Helo Wrolld!" << std::endl;
+	// Arrays are collection of elements of the same type placed in contiguous memory locations
+
+
+	int array[100] = {0}; // initialize only one will make every element 0 by default
+
+	int n;
+	std::cin >> n;
+
+	// Input
+	for (int i = 0; i < n; i++) {
+		std::cin >> array[i];
+	}
+
+	// Output
+	for (int i = 0; i < 100; i++) {
+		std::cout << array[i] << ", ";
+	}
+	std::cout << std::endl;
+
 
 	std::cin.get();
 }

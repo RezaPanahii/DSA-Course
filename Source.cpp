f28c72920040c409d2f0ef938cc703455af0babf
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-17 22:09:38 +0430

    implement reverse array function

--- Source.cpp
+++ Source.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 
+void reverseArray(int arr[], const int n) {
+	int start = 0, end = n - 1;
+	int temp = 0;
+
+	while (start < end) {
+		std::swap(arr[start], arr[end]);
+
+		start++;
+		end--;
+	}
+}
+
 int main()
 {
 	// Arrays are collection of elements of the same type placed in contiguous memory locations
 
 
-	int array[100] = {0}; // initialize only one will make every element 0 by default
-
-	int n;
-	std::cin >> n;
-
-	// Input
+	int arr[] = { 10,20,30,40,50,64,76 };
+	int n = sizeof(arr) / sizeof(int);
+	
+	std::cout << "Before modification: ";
 	for (int i = 0; i < n; i++) {
-		std::cin >> array[i];
-	}
-
-	// Output
-	for (int i = 0; i < 100; i++) {
-		std::cout << array[i] << ", ";
+		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
 
+	reverseArray(arr, n);
+
+	std::cout << "After modification: ";
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
 
 	std::cin.get();
 }

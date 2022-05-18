8197bb7f965cea4d1aa5f306b126a08255951188
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-18 22:18:59 +0430

    implement kadane's algorithm to suarray_sum problem

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,23 @@
 #include <iostream>
 
 void LargestSumSubarrays(int arr[], int n) {
-	int largest = 0;
-	int sum;
-	int prefix[n] = { 0 };
-
-	prefix[0] = arr[0];
-	for (int i = 1; i < n;i++) {
-		prefix[i] = prefix[i-1] + arr[i];
-	}
-
+	int currentSum = 0;
+	int maxSum = 0;
 	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++) {
-			sum = (i > 0) ? prefix[j] - prefix[i - 1] : prefix[j];
-			if (sum > largest) {
-				largest = sum;
-			}
-		}3 
+		currentSum += arr[i];
+		if (currentSum <= 0) {
+			currentSum = 0;
+		}
+		else if (currentSum > maxSum) {
+			maxSum = currentSum;
+		}
 	}
-	std::cout << "Largest subarray sum is " << largest << std::endl;
+	std::cout << "the largest subarray is " << maxSum << std::endl;
 }
 
 int main()
 {
-	int arr[] = { 2,4,6,-8,10,-12,-14,16,18 };
+	int arr[] = { -2,4,6,-8,-23,-10,17,-12,-14,16,-18 };
 	int size = sizeof(arr) / sizeof(int);
 
 	LargestSumSubarrays(arr, size);

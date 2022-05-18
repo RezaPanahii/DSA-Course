86444d0cbceba9d76fc2b1118a400b6895badafc
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-18 16:18:05 +0430

    solve print_subarray problem

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-// print all pairs of elements
-void PrintAllPairs(int arr[], int n) {
+void PrintSubarrays(int arr[], int n) {
 	for (int i = 0; i < n; i++) {
-		for (int j = i + 1; j < n; j++) {
-			std::cout << "(" << arr[i] << "," << arr[j] << ")" << "  ";
+		for (int j = i; j < n; j++) {
+			std::cout << "(";
+			for (int k = i; k <= j; k++) {
+				std::cout << arr[k] << ",";
+			}
+			std::cout << ")" << std::endl;
 		}
 		std::cout << std::endl;
 	}
@@ -12,10 +15,10 @@ void PrintAllPairs(int arr[], int n) {
 
 int main()
 {
-	int arr[] = { 10,17,20,30,36,40,50,64,76 };
+	int arr[] = { 2,4,6,8,10,12,14,16,18 };
 	int size = sizeof(arr) / sizeof(int);
 
-	PrintAllPairs(arr, size);
+	PrintSubarrays(arr, size);
 
 	std::cin.get();
 }

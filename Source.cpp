e7b91be8fa3c1cd6789890e823e3ec747b2d7ef9
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-18 10:02:35 +0430

    solve print_all_pairs problem

--- Source.cpp
+++ Source.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
 
-bool BinarySearch(const int arr[], const int n, const int key) {
-	int start = 0, end = n-1, mid;
-	bool isKey = false;
-
-	while (start <= end) {
-		mid = (end + start) / 2;
-		if (key < arr[mid]) {
-			end = mid - 1;
+// print all pairs of elements
+void PrintAllPairs(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			std::cout << "(" << arr[i] << "," << arr[j] << ")" << "  ";
 		}
-		else if (key == arr[mid]) {
-			std::cout << "Key found at index " << mid + 1 << std::endl;
-			isKey = true;
-			return true;
-			break;
-		}
-		else if (key > arr[mid]) {
-			start = mid + 1;
-		}
-	}
-	if (!isKey) {
-		std::cout << "Key not found!" << std::endl;
-		return false; 
+		std::cout << std::endl;
 	}
 }
 
 int main()
 {
-	// Binary search needs your search space monotonic (non decreasing or non increasing)
-
 	int arr[] = { 10,17,20,30,36,40,50,64,76 };
 	int size = sizeof(arr) / sizeof(int);
-	int key = 176;
 
-	BinarySearch(arr, size, key);
+	PrintAllPairs(arr, size);
 
 	std::cin.get();
 }

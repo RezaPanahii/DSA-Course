e5182350336843c685e620411fc343197eb5e251
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-19 12:09:49 +0430

    implement insertion sort

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 
-void BubbleSort(int arr[], int n) {
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < n - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				std::swap(arr[j], arr[j + 1]);
-			}
+void InsertionSort(int arr[], int count) {
+	int temp = 0;
+	int current = 0;
+	int prev = 0;
+	for (int i = 1; i < count; i++) {
+		current = arr[i];
+		prev = i - 1;
+		while (prev >= 0 and arr[prev] > current) {
+			arr[prev + 1] = arr[prev]; // move prev one step ahead
+			prev--; // prev points on the prev location
 		}
+		arr[prev + 1] = current; // put the element in the empty position
 	}
-	for (int i = 0; i < n; i++) {
+
+	for (int i = 0; i < count; i++) {
 		std::cout << arr[i] << " ";
 	}
 	std::cout << std::endl;
@@ -16,10 +22,10 @@ void BubbleSort(int arr[], int n) {
 
 int main()
 {
-	int arr[] = { 3,7,2,5,6,2,1 };
+	int arr[] = { 10,7,2,5,6,2,1 };
 	int size = sizeof(arr) / sizeof(int);
 
-	BubbleSort(arr, size);
+	InsertionSort(arr, size);
 
 	std::cin.get();
 }

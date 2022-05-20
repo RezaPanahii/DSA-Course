953d77644c145736b3bf93880e01452767c38b8b
Reza Panahi <rezapanahiads2@gmail.com>
2022-05-20 14:43:11 +0430

    read char[] using cin.get

--- Source.cpp
+++ Source.cpp
@@ -1,51 +1,31 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
-
-using std::cout;
-using std::endl;
-
-// works only with arrays of positive values
-void CountingSort(std::vector<int>& arr) {
-	int max = 0;
-
-	// findinf maximum element
-	for (int i = 0; i < arr.size(); i++) {
-		if (arr[i] > max) {
-			max = arr[i];
-		}
-	}
-
-	// create a count array
-	std::vector<int>  frequency(max+1, 0);
-
-	// update frequency array
-	for (int x : arr) {
-		frequency[x]++;
-	}
-
-	// put back elements from frequency to arr
-	int j = 0;
-	for (int i = 0; i < max+1; i++) {
-		while (frequency[i] > 0) {
-			arr[j] = i; // putting back values to arr
-			frequency[i]--; // decrese value of frequency
-			j++; // increase position of arr
-		}
-	}
-
-	// print arr
-	for (int x : arr) {
-		cout << x << " ";
-	}
-	cout << endl;
-}
+#include <cstring>
+using namespace std;
 
 int main()
 {
-	std::vector<int> arr = { 10,7,12,5,5,32,61,2,1 };
+    system( "cls" );
+    char sentence[1000];
 
-	CountingSort(arr);
-	
-	std::cin.get();
+    char temp = cin.get();
+
+    int length = 0;
+    while (temp != '@') {
+        sentence[length++] = temp;
+        temp = cin.get();
+    }
+    sentence[length] = '\0';
+
+    cout << sentence << endl;
+    cout << length << endl;
+
+    return 0;
 }
+
+
+// template <typename R>
+// R function( R a, R b ) {
+//     T result = a + b;
+
+//     return result;
+// }
